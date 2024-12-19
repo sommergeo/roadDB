6ec8c557e2df620cb063b7a67eb1ee059336198a
ZaraKxxxxxx <kanaeva.zara@gmail.com>
2024-12-19 18:48:09 +0100

    attributeName -> attribute_name

--- roadDB/R/road_package_aux.R
+++ roadDB/R/road_package_aux.R
@@ -5,17 +5,17 @@ library(RPostgres)
 attributes <- c("type", "continent", "continent_region", "country")
 tables <- c("locality", "geopolitical_units", "geopolitical_units", "geopolitical_units")
 
-road_list_values <- function (attributeName = NULL)
+road_list_values <- function (attribute_name = NULL)
 {
   # computing length of attributes
   size = length(attributes)
   # iterating over elements of attributes
   for (i in 1:size){
-    if(attributeName == attributes[i]){
+    if(attribute_name == attributes[i]){
       table = tables[i]
     }
   }  
-  query <- paste( "SELECT DISTINCT ", attributeName, " FROM ", table, " ORDER BY ", attributeName)
+  query <- paste( "SELECT DISTINCT ", attribute_name, " FROM ", table, " ORDER BY ", attribute_name)
   
   data <- road_run_query(query)
   
