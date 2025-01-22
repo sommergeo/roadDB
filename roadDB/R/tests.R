2168c3b7d6cfa5ed6ba3a925570c2a5ca7268a22
ZaraKxxxxxx <kanaeva.zara@gmail.com>
2025-01-22 14:16:27 +0100

    .

--- roadDB/R/tests.R
+++ roadDB/R/tests.R
@@ -2,14 +2,14 @@
 
 # road_get_localities <- function(continent = NULL, subcontinent = NULL, country = NULL, locality_type = NULL)
 
-localities1 <- road_get_localities(continent = "Africa")
-localities1 <- road_get_localities(continent = c('Africa', "Asia"))
-# localities11 <- road_get_localities(continent = "Africa", country = "South Africa")
+localities1 <- road_get_localities(continents = "Africa")
+localities1 <- road_get_localities(continents = c('Africa', "Asia"))
+# localities11 <- road_get_localities(continents = "Africa", country = "South Africa")
 
 
 # road_get_assemblages <- function(localities, category = NULL, age_min = NULL, age_max = NULL)
 
-assemblages1 <- road_get_assemblages(localities1)
+assemblages1 <- road_get_assemblages(localities = localities1)
 # assemblages11 <- road_get_assemblages(localities1, category = "human remains")
 
 # road_get_human_remains <- function(assemblages, genus = NULL, species = NULL, genus_species = NULL)
