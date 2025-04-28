a882f1be89d5ba73d5804c1ea8e6311cc2f42e89
Timo <timo.streicher@googlemail.com>
2025-04-28 20:11:33 +0200

    Tests for get_archaeology part2.

--- tests/testthat/test-get_archaeology.R
+++ tests/testthat/test-get_archaeology.R
@@ -32,10 +32,15 @@ test_that("road_get_lithic_typology()", {
   )
 
   # Filter for "flakes" in tool_list column
-  result_flakes <- road_get_lithic_typology(tool_list = "flake")
+  result_filter1 <- road_get_lithic_typology(tool_list = "flake")
   expect_true(
-    all(grepl("flake", result_flakes$tool_list, ignore.case = TRUE)),
+    all(grepl("flake", result_filter1$tool_list, ignore.case = TRUE)),
     info = "Some values in the tool_list column do not contain the string 'flake'"
+  )
+  # Check if there is at least one row in the result
+  expect_true(
+    nrow(result_filter1) > 0,
+    info = "The result does not contain any rows"
   )
 })
 
@@ -73,11 +78,16 @@ test_that("road_get_lithic_raw_material()", {
     info = "Some values in the percentage column are outside the expected range (0 to 100)"
   )
 
-  # Filter for "flint" in tool_list column
-  result_flint <- road_get_lithic_typology(tool_list = "flint")
+  # Filter for "flint" in raw_material_list column
+  result_filter1 <- road_get_lithic_raw_material(raw_material_list = "flint")
   expect_true(
-    all(grepl("flint", result_flint$tool_list, ignore.case = TRUE)),
-    info = "Some values in the tool_list column do not contain the string 'flint'"
+    all(grepl("flint", result_filter1$raw_material_list, ignore.case = TRUE)),
+    info = "Some values in the raw_material_list column do not contain the string 'flint'"
+  )
+  # Check if there is at least one row in the result
+  expect_true(
+    nrow(result_filter1) > 0,
+    info = "The result does not contain any rows"
   )
 })
 
@@ -114,23 +124,128 @@ test_that("road_get_organic_tools()", {
     all(grepl("abrader/polisher", result_filter1$organic_tools_interpretation, ignore.case = TRUE)),
     info = "Some values in the organic_tools_interpretation column do not contain the string 'abrader/polisher'"
   )
+  # Check if there is at least one row in the result
+  expect_true(
+    nrow(result_filter1) > 0,
+    info = "The result does not contain any rows"
+  )
 })
 
 
-# road_get_lithic_typology()
-#lithic_typology1 <- road_get_lithic_typology(continents = "Europe", tool_list = "biface")
+test_that("road_get_symbolic_artifacts()", {
+  # Call function without any filters
+  result_all_columns <- road_get_symbolic_artifacts()
 
-#road_get_lithic_raw_material()
-#lithic_raw_material1 <- road_get_lithic_raw_material(countries = "France", raw_material_list = c("volcanic rock", "obsidian"))
+  # Check if the column types match the expected types
+  expect_equal(class(result_all_columns$locality_id), "character")
+  expect_equal(class(result_all_columns$assemblage_id), "integer")
+  expect_equal(class(result_all_columns$continent), "character")
+  expect_equal(class(result_all_columns$subcontinent), "character")
+  expect_equal(class(result_all_columns$country), "character")
+  expect_equal(class(result_all_columns$locality_types), "character")
+  expect_equal(class(result_all_columns$coord_x), "numeric")
+  expect_equal(class(result_all_columns$coord_y), "numeric")
+  expect_equal(class(result_all_columns$assemblage_name), "character")
+  expect_equal(class(result_all_columns$categories), "character")
+  expect_equal(class(result_all_columns$age_min), "integer")
+  expect_equal(class(result_all_columns$age_max), "integer")
+  expect_equal(class(result_all_columns$cultural_periods), "character")
+  expect_equal(class(result_all_columns$symbolic_artifacts_interpretation), "character")
+  expect_equal(class(result_all_columns$symbolic_artifacts_category), "character")
+  expect_equal(class(result_all_columns$symbolic_artifacts_material), "character")
+  expect_equal(class(result_all_columns$symbolic_artifacts_raw_material_source), "character")
+  expect_equal(class(result_all_columns$comments), "character")
 
-#road_get_organic_tools()
-#organic_tools1 <- road_get_organic_tools(continents = "Europe", organic_tools_interpretation = "abrader/polisher")
+  # Check if the result is a data frame and has the expected number of rows and columns
+  expect_s3_class(result_all_columns, "data.frame")
+  expect_equal(ncol(result_all_columns), 19)
 
-#road_get_symbolic_artifacts()
-#symbolic_artifacts1 <- road_get_symbolic_artifacts(continents = "Europe", symbolic_artifacts_interpretation = "abstract", cultural_periods = "Middle Paleolithic")
+  result_filter1 <- road_get_symbolic_artifacts(symbolic_artifacts_interpretation = "abstract")
+  expect_true(
+    all(grepl("abstract", result_filter1$symbolic_artifacts_interpretation, ignore.case = TRUE)),
+    info = "Some values in the symbolic_artifacts_interpretation column do not contain the string 'abstract'"
+  )
+  # Check if there is at least one row in the result
+  expect_true(
+    nrow(result_filter1) > 0,
+    info = "The result does not contain any rows"
+  )
+})
 
-#road_get_feature()
-#feature1 <- road_get_feature(continents = "Europe", feature_interpretation = "bedding", cultural_periods = "Middle Paleolithic")
 
-#road_get_miscellaneous_finds()
-#miscellaneous_finds1 <- road_get_miscellaneous_finds(continents = "Europe", miscellaneous_finds_material = "wood fossil", cultural_periods = "Middle Paleolithic")
+test_that("road_get_feature()", {
+  # Call function without any filters
+  result_all_columns <- road_get_feature()
+
+  # Check if the column types match the expected types
+  expect_equal(class(result_all_columns$locality_id), "character")
+  expect_equal(class(result_all_columns$assemblage_id), "integer")
+  expect_equal(class(result_all_columns$continent), "character")
+  expect_equal(class(result_all_columns$subcontinent), "character")
+  expect_equal(class(result_all_columns$country), "character")
+  expect_equal(class(result_all_columns$locality_types), "character")
+  expect_equal(class(result_all_columns$coord_x), "numeric")
+  expect_equal(class(result_all_columns$coord_y), "numeric")
+  expect_equal(class(result_all_columns$assemblage_name), "character")
+  expect_equal(class(result_all_columns$categories), "character")
+  expect_equal(class(result_all_columns$age_min), "integer")
+  expect_equal(class(result_all_columns$age_max), "integer")
+  expect_equal(class(result_all_columns$cultural_periods), "character")
+  expect_equal(class(result_all_columns$feature_interpretation), "character")
+  expect_equal(class(result_all_columns$comments), "character")
+
+  # Check if the result is a data frame and has the expected number of rows and columns
+  expect_s3_class(result_all_columns, "data.frame")
+  expect_equal(ncol(result_all_columns), 15)
+
+  result_filter1 <- road_get_feature(feature_interpretation = "bedding")
+  expect_true(
+    all(grepl("bedding", result_filter1$feature_interpretation, ignore.case = TRUE)),
+    info = "Some values in the feature_interpretation column do not contain the string 'bedding'"
+  )
+  # Check if there is at least one row in the result
+  expect_true(
+    nrow(result_filter1) > 0,
+    info = "The result does not contain any rows"
+  )
+})
+
+
+test_that("road_get_miscellaneous_finds()", {
+  # Call function without any filters
+  result_all_columns <- road_get_miscellaneous_finds()
+
+  # Check if the column types match the expected types
+  expect_equal(class(result_all_columns$locality_id), "character")
+  expect_equal(class(result_all_columns$assemblage_id), "integer")
+  expect_equal(class(result_all_columns$continent), "character")
+  expect_equal(class(result_all_columns$subcontinent), "character")
+  expect_equal(class(result_all_columns$country), "character")
+  expect_equal(class(result_all_columns$locality_types), "character")
+  expect_equal(class(result_all_columns$coord_x), "numeric")
+  expect_equal(class(result_all_columns$coord_y), "numeric")
+  expect_equal(class(result_all_columns$assemblage_name), "character")
+  expect_equal(class(result_all_columns$categories), "character")
+  expect_equal(class(result_all_columns$age_min), "integer")
+  expect_equal(class(result_all_columns$age_max), "integer")
+  expect_equal(class(result_all_columns$cultural_periods), "character")
+  expect_equal(class(result_all_columns$miscellaneous_finds_material), "character")
+  expect_equal(class(result_all_columns$miscellaneous_finds_raw_material_source), "character")
+  expect_equal(class(result_all_columns$number), "integer")
+  expect_equal(class(result_all_columns$comments), "character")
+
+  # Check if the result is a data frame and has the expected number of rows and columns
+  expect_s3_class(result_all_columns, "data.frame")
+  expect_equal(ncol(result_all_columns), 17)
+
+  result_filter1 <- road_get_miscellaneous_finds(miscellaneous_finds_material = "wood fossil")
+  expect_true(
+    all(grepl("wood fossil", result_filter1$miscellaneous_finds_material, ignore.case = TRUE)),
+    info = "Some values in the miscellaneous_finds_material column do not contain the string 'wood fossil'"
+  )
+  # Check if there is at least one row in the result
+  expect_true(
+    nrow(result_filter1) > 0,
+    info = "The result does not contain any rows"
+  )
+})
