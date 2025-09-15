3878603af6c2f88e830ad908acccf3da2ce48fac
Timo <timo.streicher@googlemail.com>
2025-09-15 12:54:25 +0200

    Return values description of localities, humanremains, plantremains, paleofauna.

--- R/get_plantremains.R
+++ R/get_plantremains.R
@@ -2,8 +2,8 @@
 #'
 #' The function  \strong{\code{road_get_plantremains}} fetches data of paleobotanical remains from the ROAD database.
 #' Paleobotanical remains are plant remains found in archaeological contexts and are associated with
-#' assemblages. 
-#' 
+#' assemblages.
+#'
 #' @details
 #' This function allows you to query paleobotanical data based on
 #' various parameters such as geographical location, cultural periods, plant taxonomy,
@@ -14,65 +14,68 @@
 #'
 #' @param continents specifies the continent(s) (e.g. Africa, Europe, Asia).
 #' Run \code{road_list_argument_values("continents")} to display possible values.
-#' The argument \code{continents} is a string (one item) or vector of strings 
+#' The argument \code{continents} is a string (one item) or vector of strings
 #' (one or more items); defaults to NULL.
-#' @param subcontinents specifies the continental region(s) (e.g. Southern Europe). 
+#' @param subcontinents specifies the continental region(s) (e.g. Southern Europe).
 #' Run \code{road_list_argument_values("subcontinents")} to display possible values.
-#' The argument \code{subcontinents} is a string (one item) or vector of strings 
+#' The argument \code{subcontinents} is a string (one item) or vector of strings
 #' (one or more items); defaults to NULL.
-#' @param countries specifies the name of the country where a locality is situated 
-#' (e.g. Germany, Kenya, Saudi Arabia). Run \code{road_list_argument_values("countries")} 
+#' @param countries specifies the name of the country where a locality is situated
+#' (e.g. Germany, Kenya, Saudi Arabia). Run \code{road_list_argument_values("countries")}
 #' to display possible values.
-#' The argument \code{countries} is a string (one item) or vector of strings 
+#' The argument \code{countries} is a string (one item) or vector of strings
 #' (one or more items); defaults to NULL.
 #' @param locality_types specifies the type of locality (e.g. cave, rockshelter, open air).
 #' Run \code{road_list_argument_values("locality_types")} to display possible values.
-#' The argument \code{locality_types} is a string (one item) or vector of strings 
+#' The argument \code{locality_types} is a string (one item) or vector of strings
 #' (one or more items); defaults to NULL.
-#' @param cultural_periods specifies the main cultural epoch(s) and includes the 
-#' Eurasian Paleolithic (Lower, Middle, Upper, Epi-) and the African Stone Age 
-#' (Earlier, Middle, Later). Run \code{road_list_argument_values("cultural_periods")} 
-#' to display possible values. The argument \code{cultural_periods} is a string 
+#' @param cultural_periods specifies the main cultural epoch(s) and includes the
+#' Eurasian Paleolithic (Lower, Middle, Upper, Epi-) and the African Stone Age
+#' (Earlier, Middle, Later). Run \code{road_list_argument_values("cultural_periods")}
+#' to display possible values. The argument \code{cultural_periods} is a string
 #' (one item) or vector of strings (one or more items); defaults to NULL.
-#' @param technocomplexes specifies an archaeological culture or named stone tool 
+#' @param technocomplexes specifies an archaeological culture or named stone tool
 #' industry (e.g. Oldowan, Acheulean, Mousterian).
 #' Run \code{road_list_argument_values("technocomplexes")} to display possible values.
-#' The argument \code{technocomplexes} is a string (one item) or vector of strings 
+#' The argument \code{technocomplexes} is a string (one item) or vector of strings
 #' (one or more items); defaults to NULL.
-#' @param categories specifies the assemblage category/categories with the classes 
-#' human remains, raw material, typology, technology, function, organic tools, 
-#' symbolic artifacts, feature, miscellaneous finds, paleofauna, animal remains, 
-#' plant remains. The argument \code{categories} is a string (one item) or 
+#' @param categories specifies the assemblage category/categories with the classes
+#' human remains, raw material, typology, technology, function, organic tools,
+#' symbolic artifacts, feature, miscellaneous finds, paleofauna, animal remains,
+#' plant remains. The argument \code{categories} is a string (one item) or
 #' vector of strings (one or more items); defaults to NULL.
-#' @param age_min specifies the minimum age in years before present, using 1950 CE 
+#' @param age_min specifies the minimum age in years before present, using 1950 CE
 #' as the baseline. The argument \code{age_min} is an integer; defaults to NULL.
-#' @param age_max specifies the maximum age in years before present, using 1950 CE 
+#' @param age_max specifies the maximum age in years before present, using 1950 CE
 #' as the baseline. The argument \code{age_max} is an integer; defaults to NULL.
-#' @param assemblages list of assemblages; return value from function 
-#' \code{road_get_assemblages}. Can be used instead of the other locality and 
+#' @param assemblages list of assemblages; return value from function
+#' \code{road_get_assemblages}. Can be used instead of the other locality and
 #' assemblage parameters to filter the results.
-#' @param plant_remains specifies the type of plant remains. Possible entries 
-#' include: "pollen", "plant macroremains" etc. Run 
-#' \code{road_list_argument_values("plant_remains")} to display 
-#' possible values. The argument \code{plant_remains} is a string (one item) or vector of 
+#' @param plant_remains specifies the type of plant remains. Possible entries
+#' include: "pollen", "plant macroremains" etc. Run
+#' \code{road_list_argument_values("plant_remains")} to display
+#' possible values. The argument \code{plant_remains} is a string (one item) or vector of
 #' strings; defaults to NULL.
-#' @param plant_family specifies the family to which the described plant remains 
+#' @param plant_family specifies the family to which the described plant remains
 #' is attributed to. Possible entries include: "Poaceae", "Typhaceae" etc.
-#' Run \code{road_list_argument_values("plant_family")} to display 
-#' possible values. The argument \code{plant_family} is a string (one item) or 
+#' Run \code{road_list_argument_values("plant_family")} to display
+#' possible values. The argument \code{plant_family} is a string (one item) or
 #' vector of strings; defaults to NULL.
 #' @param plant_genus specifies the genus name of the taxon.
-#' Run \code{road_list_argument_values("plant_genus")} to display 
-#' possible values. The argument \code{plant_genus} is a string (one item) or 
+#' Run \code{road_list_argument_values("plant_genus")} to display
+#' possible values. The argument \code{plant_genus} is a string (one item) or
 #' vector of strings; defaults to NULL.
-#' @param plant_species specifies the full and valid species name including 
-#' author’s name (e.g. Quercus ilex L., Sciadopitys verticillata (Thunb.) 
-#' Siebold & Zucc). Run \code{road_list_argument_values("plant_species")} 
-#' to display possible values. The argument \code{plant_species} is a string 
+#' @param plant_species specifies the full and valid species name including
+#' author’s name (e.g. Quercus ilex L., Sciadopitys verticillata (Thunb.)
+#' Siebold & Zucc). Run \code{road_list_argument_values("plant_species")}
+#' to display possible values. The argument \code{plant_species} is a string
 #' (one item) or vector of strings; defaults to NULL.
 #'
-#' @return Database search result as a list of assemblages with information about 
-#' paleobotanical remains.
+#' @return A data frame with plant remains information. Rows represent individual
+#' plant remains finds, columns contain standard outputs and plant remains-related details on:
+#' @return \code{plant_remains}: The attribute specifies the type of plant remains (e.g. pollen, plant macroremains).
+#' @return \code{plant_family}, \code{plant_genus}, \code{plant_species}: The attributes specify the taxonomic classification of the plant remains.
+#'
 #' @export
 #'
 #' @examples
